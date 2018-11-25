4ea7ccaa68bcc5974681f4e00e812de23ec6d77a
Alan Ficagna <alan.ficagna@gmail.com>
2018-11-25 10:02:32 -0200

    Config prod.

--- config/prod.exs
+++ config/prod.exs
@@ -12,10 +12,19 @@ use Mix.Config
 config :app, AppWeb.Endpoint,
   http: [:inet6, port: System.get_env("PORT") || 4000],
   url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  server: true,
+  secret_key_base: "${SECRET_KEY_BASE}"
 
 # Do not print debug messages in production
 config :logger, level: :info
+
+config :app, App.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: "${DATABASE_URL}",
+  database: "",
+  pool_size: 1,
+  ssl: true
 
 # ## SSL Support
 #
@@ -68,4 +77,4 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs which should be versioned
 # separately.
-import_config "prod.secret.exs"
+# import_config "prod.secret.exs"
